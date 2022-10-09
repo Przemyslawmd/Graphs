56a4ba5090f3ee6820f97521a917e56ee2cbaf01
przemyslawmd <przemyslawmd@gmail.com>
2022-10-09 23:43:08 +0200

    DFS implementation

---
+++ src/dfs/dfs.cpp
@@ -0,0 +1,34 @@
+
+#include "dfs.h"
+
+#include <iterator>
+
+
+DFS::DFS(Graph& graph) : adjacency(graph.getAdjacencyList()), nodes(graph.getNodes()) {}
+
+
+void DFS::traverseGraph()
+{
+    char initialKey = adjacency.at(0).at(0);        
+    processNode(initialKey);   
+}
+
+
+void DFS::processNode(char key)
+{
+    auto itNode = std::find_if(nodes.begin(), nodes.end(), [&](const auto& node) { return node->getKey() == key; });
+    (*itNode)->setState(State::COMPLETED);
+
+    auto itNeighbours = std::find_if(adjacency.begin(), adjacency.end(), [&](const auto& vec) { return vec.at(0) == key; });
+    std::vector<char>::iterator it = itNeighbours->begin();
+    std::advance(it, 1);
+     
+    while (it != itNeighbours->end()) {
+        auto itNode = std::find_if(nodes.begin(), nodes.end(), [&](const auto& node) { return node->getKey() == *it; });
+        if ((*itNode).get()->getState() == State::NOT_VISITED) {
+            processNode(*it);
+        }
+        it++;
+    }
+}
+
---
+++ src/dfs/dfs.h
@@ -0,0 +1,30 @@
+
+#ifndef GRAPHS_DFS_H
+#define GRAPHS_DFS_H
+
+#include "../graph/node.h"
+#include "../graph/graph.h"
+
+#include <memory>
+#include <queue>
+#include <vector>
+
+
+class DFS
+{
+public:
+
+    DFS(Graph&);
+
+    void traverseGraph();
+
+private:
+
+    void processNode(char key);
+
+    std::vector<std::vector<char>>& adjacency;
+    std::vector<std::unique_ptr<Node>>& nodes;
+};
+
+#endif
+
