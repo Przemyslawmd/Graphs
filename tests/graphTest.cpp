0a6891b909e63f1648650f04cc288b95a1202586
przemyslawmd <przemyslawmd@gmail.com>
2023-03-08 01:02:51 +0100

    Refactoring graphTest

--- tests/graphTest.cpp
+++ tests/graphTest.cpp
@@ -9,32 +9,43 @@
 #include <vector>
  
 
-void checkAdjacency(const std::map<char, 
-                    std::list<Edge>>& adjacency,
+void checkAdjacency(const std::map<char, std::list<Edge>>& adjacency,
                     char key,
-                    const std::vector<char>& neighboursVec)
+                    const std::vector<char>& neighbours)
 {
-    ASSERT_EQ(adjacency.at(key).size(), neighboursVec.size());
+    ASSERT_EQ(adjacency.at(key).size(), neighbours.size());
 
-    auto it = adjacency.at(key).begin();
-    for (char neighbour : neighboursVec) {
+    auto it = adjacency.at(key).cbegin();
+    for (char neighbour : neighbours) {
         EXPECT_EQ(it->dst, neighbour);
         std::advance(it, 1);
     }
 }
 
 
-void checkAdjacencyWithWeight(const std::map<char, 
-                              std::list<Edge>>& adjacency, 
+void checkAdjacencyWithWeight(const std::map<char, std::list<Edge>>& adjacency,
                               char key,
-                              const std::vector<std::tuple<char, int>>& edgesVec)
+                              const std::vector<std::tuple<char, int>>& edges)
 {
-    ASSERT_EQ(adjacency.at(key).size(), edgesVec.size());
+    ASSERT_EQ(adjacency.at(key).size(), edges.size());
 
-    auto it = adjacency.at(key).begin();
-    for (const auto& edge : edgesVec) {
+    auto it = adjacency.at(key).cbegin();
+    for (const auto& edge : edges) {
         EXPECT_EQ(it->dst, std::get<0>(edge));
         EXPECT_EQ(it->weight, std::get<1>(edge));
+        std::advance(it, 1);
+    }
+}
+
+
+void checkNodes(const std::vector<std::unique_ptr<Node>>& nodes,
+                const std::vector<char>& keys)
+{
+    ASSERT_EQ(nodes.size(), keys.size());
+
+    auto it = nodes.cbegin();
+    for (const auto key : keys) {
+        EXPECT_EQ(key, (*it)->key);
         std::advance(it, 1);
     }
 }
@@ -53,20 +64,10 @@ TEST(TestCreateGraph, FirstTest)
     graph.addEdges('e', { 'c', 'd' });
 
     const auto& nodes = graph.getNodes();
-
-    ASSERT_EQ(nodes[0]->key, 'a');
-    ASSERT_EQ(nodes[1]->key, 'b');
-    ASSERT_EQ(nodes[2]->key, 'c');
-    ASSERT_EQ(nodes[3]->key, 'd');
-    ASSERT_EQ(nodes[4]->key, 'e');
+    checkNodes(nodes, { 'a', 'b', 'c', 'd', 'e' });
 
     const auto& adjacency = graph.getAdjacency();
-
     ASSERT_EQ(adjacency.size(), 5);
-    for (char key : keys) {
-        ASSERT_EQ(adjacency.count(key), 1);
-    }
-
     checkAdjacency(adjacency, 'a', { 'b' });
     checkAdjacency(adjacency, 'b', { 'a', 'c' });
     checkAdjacency(adjacency, 'c', { 'b', 'd', 'e' });
@@ -75,41 +76,29 @@ TEST(TestCreateGraph, FirstTest)
 }
 
 
-TEST(TestCreateGraph, SecondTestImproperEdges)
+TEST(TestCreateGraph, SecondTestNoNodesCreatedBefore)
 {
-    std::vector<char> keys = { 'a', 'b', 'c', 'd', 'e' };
     Graph graph{ false, false };
-    graph.addNodes(keys);
 
     graph.addEdges('a', { 'b' });
     graph.addEdges('b', { 'a', 'c' });
     graph.addEdges('c', { 'b', 'd', 'e' });
-    graph.addEdges('a', { 'b' });                // repeat edge
+    graph.addEdges('a', { 'b' });                // repeated edge
     graph.addEdges('d', { 'c', 'e' });
-    graph.addEdges('F', { 'e' });                // improper source key
+    graph.addEdges('f', { 'e' });
     graph.addEdges('e', { 'c', 'd'});
-    graph.addEdges('d', { 'G' });                // improper destination key
+    graph.addEdges('d', { 'g' });
 
     const auto& nodes = graph.getNodes();
+    checkNodes(nodes, { 'a', 'b', 'c', 'd', 'e', 'f', 'g' });
     
-    ASSERT_EQ(nodes[0]->key, 'a');
-    ASSERT_EQ(nodes[1]->key, 'b');
-    ASSERT_EQ(nodes[2]->key, 'c');
-    ASSERT_EQ(nodes[3]->key, 'd');
-    ASSERT_EQ(nodes[4]->key, 'e');
-
     const auto& adjacency = graph.getAdjacency();
-
     ASSERT_EQ(adjacency.size(), 7);
-    for (char key : keys) {
-        ASSERT_EQ(adjacency.count(key), 1);    
-    }
-    
     checkAdjacency(adjacency, 'a', { 'b' });
     checkAdjacency(adjacency, 'b', { 'a', 'c' });
     checkAdjacency(adjacency, 'c', { 'b', 'd', 'e' });
-    checkAdjacency(adjacency, 'd', { 'c', 'e', 'G' });
-    checkAdjacency(adjacency, 'e', { 'c', 'd', 'F' });
+    checkAdjacency(adjacency, 'd', { 'c', 'e', 'g' });
+    checkAdjacency(adjacency, 'e', { 'c', 'd', 'f' });
 }
 
 
@@ -126,20 +115,10 @@ TEST(TestCreateGraph, ThirdTestGraphWithWeights)
     graph.addEdgesWeighted('e', {{ 'c', 3  }, { 'd', 101 }});
 
     const auto& nodes = graph.getNodes();
-    
-    ASSERT_EQ(nodes[0]->key, 'a');
-    ASSERT_EQ(nodes[1]->key, 'b');
-    ASSERT_EQ(nodes[2]->key, 'c');
-    ASSERT_EQ(nodes[3]->key, 'd');
-    ASSERT_EQ(nodes[4]->key, 'e');
+    checkNodes(nodes, { 'a', 'b', 'c', 'd', 'e' });
 
     const auto& adjacency = graph.getAdjacency();
-
     ASSERT_EQ(adjacency.size(), 5);
-    for (char key : keys) {
-        ASSERT_EQ(adjacency.count(key), 1);
-    }
-
     checkAdjacencyWithWeight(adjacency, 'a', {{ 'b', 1  }});
     checkAdjacencyWithWeight(adjacency, 'b', {{ 'a', 2  }, { 'c', 4   }});
     checkAdjacencyWithWeight(adjacency, 'c', {{ 'b', 10 }, { 'd', 4   }, { 'e', 1 }});
