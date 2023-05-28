d31bd1ea9c436b1e41e6c50601397c98fe56cd03
przemyslawmd <przemyslawmd@gmail.com>
2023-05-28 21:41:53 +0200

    [DFS tests] Using graph factory; additional tests, time measurement option

--- tests/dfsTest.cpp
+++ tests/dfsTest.cpp
@@ -2,27 +2,26 @@
 #include "../src/algo/dfs.h"
 #include "../src/graph/node.h"
 #include "../src/graph/graph.h"
+#include "graphFactory.h"
 
 #include <gtest/gtest.h>
 
+#include <chrono>
 #include <memory>
 #include <vector>
+
+constexpr bool measurement = false;
 
 
 class DFSTest : public ::testing::Test
 {
 protected:
 
-    void checkNodes(const std::vector<std::unique_ptr<Node>>& nodes,
-        const std::vector<char>& keys)
+    void checkNodes(const std::vector<std::unique_ptr<Node>>& nodes, int expectedNodesCount)
     {
-        ASSERT_EQ(nodes.size(), keys.size());
-
-        auto it = nodes.cbegin();
-        for (const auto key : keys) {
-            EXPECT_EQ(key, (*it)->key);
-            EXPECT_TRUE((*it)->isVisited());
-            std::advance(it, 1);
+        ASSERT_EQ(nodes.size(), expectedNodesCount);
+        for (auto& node : nodes) {
+            EXPECT_TRUE(node->isVisited());
         }
     }
 };
@@ -31,37 +30,65 @@ protected:
 TEST_F(DFSTest, 1)
 {
     Graph graph{ false, false };
-
-    graph.addEdges('a', { 'b' });
-    graph.addEdges('b', { 'a', 'c' });
-    graph.addEdges('c', { 'b', 'd', 'e' });
-    graph.addEdges('d', { 'c', 'e' });
-    graph.addEdges('e', { 'c', 'd' });
+    GraphFactory::createGraph(graph, 1);
 
     DFS dfs{ graph };
     dfs.traverseGraph();
 
     const auto& nodes = graph.getNodes();
-    checkNodes(nodes, { 'a', 'b', 'c', 'd', 'e' });
+    checkNodes(nodes, 5);
 }    
 
 
 TEST_F(DFSTest, SecondTest)
 {
     Graph graph{ false, false };
-
-    graph.addEdges('a', { 'b', 'd' });
-    graph.addEdges('b', { 'a', 'c', 'd', 'g' });
-    graph.addEdges('c', { 'b', 'g' });
-    graph.addEdges('d', { 'a', 'b', 'f' });
-    graph.addEdges('e', { 'f' });
-    graph.addEdges('f', { 'd', 'e' });
-    graph.addEdges('g', { 'b', 'c' });
+    GraphFactory::createGraph(graph, 2);
 
     DFS dfs{ graph };
     dfs.traverseGraph();
     
     const auto& nodes = graph.getNodes();
-    checkNodes(nodes, { 'a', 'b', 'd', 'c', 'g', 'f', 'e' });
+    checkNodes(nodes, 7);
+}
+
+
+TEST_F(DFSTest, ThirdTest)
+{
+    Graph graph{ true, false };
+    GraphFactory::createGraph(graph, 3);
+
+    auto begin = std::chrono::high_resolution_clock::now();
+    DFS dfs{ graph };
+    dfs.traverseGraph();
+
+    if (measurement) {
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::cout << "DFSTest : ThirdTest : time in microseconds : " << elapsed.count() << std::endl;
+    }
+
+    const auto& nodes = graph.getNodes();
+    checkNodes(nodes, 14);
+}
+
+
+TEST_F(DFSTest, FourthTest)
+{
+    Graph graph{ false, false };
+    GraphFactory::createGraph(graph, 4);
+
+    auto begin = std::chrono::high_resolution_clock::now();
+    DFS dfs{ graph };
+    dfs.traverseGraph();
+
+    if (measurement) {
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::cout << "DFSTest : FourthTest : time in microseconds : " << elapsed.count() << std::endl;
+    }
+
+    const auto& nodes = graph.getNodes();
+    checkNodes(nodes, 20);
 }
 
