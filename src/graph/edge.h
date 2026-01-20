6c6cc2007145533d3b26d9d273ab72d7fa4889c3
Przemyslaw Madej <przemyslawmd@gmail.com>
2026-01-20 23:06:01 +0100

    [Edge] Style changes

--- src/graph/edge.h
+++ src/graph/edge.h
@@ -9,12 +9,12 @@ class Edge
 {
 public:
 
-    Edge(char src, char dst, size_t weight) : src(src), dst(dst), weight(weight) {}
+    Edge(char src, char dst, size_t weight) : src{ src }, dst{ dst }, weight{ weight } {}
 
     bool operator==(const Edge& edge);
 
     const char src;
-    char dst;
+    const char dst;
     const size_t weight;
 };
 
